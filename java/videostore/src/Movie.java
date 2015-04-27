9fd9e708acf2dbcf62deb8b55f40526a9159be38
= <chrisjordan1987@gmail.com>
2015-04-27 12:24:38 +0100

    Moved amount calculation logic to Movie.

--- java/videostore/src/Movie.java
+++ java/videostore/src/Movie.java
@@ -1,27 +1,45 @@
-public class Movie
-{
-	public static final int CHILDRENS	= 2;
-	public static final int REGULAR 	= 0;
-	public static final int NEW_RELEASE = 1;
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
 
-	private String title;
-	private int priceCode;
+    private String title;
+    private int priceCode;
 
-	public Movie (String title, int priceCode) {
-		this.title 		= title;
-		this.priceCode 	= priceCode;
-	}
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
 
-	public int getPriceCode () {
-		return priceCode;
-	}
+    public int getPriceCode() {
+        return priceCode;
+    }
 
-	public void setPriceCode (int code) {
-		priceCode = code;
-	}
+    public void setPriceCode(int code) {
+        priceCode = code;
+    }
 
-	public String getTitle () {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
+    double determineMovieAmount(int daysRented) {
+        double thisAmount = 0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
 }
