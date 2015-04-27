e058484a129f979b6e45fa7e14fd911179f5b65c
= <chrisjordan1987@gmail.com>
2015-04-27 11:01:52 +0100

    Replaced iterator with for-each loop

--- java/videostore/src/Customer.java
+++ java/videostore/src/Customer.java
@@ -19,37 +19,36 @@ public class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        ListIterator<Rental> rentals = this.rentals.listIterator();
         String result = "Rental Record for " + getName() + "\n";
 
-        while (rentals.hasNext()) {
+        for (Rental rental : rentals) {
+
             double thisAmount = 0;
-            Rental each = rentals.next();
 
             // determines the amount for each line
-            switch (each.getMovie().getPriceCode()) {
+            switch (rental.getMovie().getPriceCode()) {
                 case Movie.REGULAR:
                     thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    if (rental.getDaysRented() > 2)
+                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
                     break;
                 case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
+                    thisAmount += rental.getDaysRented() * 3;
                     break;
                 case Movie.CHILDRENS:
                     thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    if (rental.getDaysRented() > 3)
+                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
                     break;
             }
 
             frequentRenterPoints++;
 
-            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE
-                    && each.getDaysRented() > 1)
+            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
+                    && rental.getDaysRented() > 1)
                 frequentRenterPoints++;
 
-            result += "\t" + each.getMovie().getTitle() + "\t"
+            result += "\t" + rental.getMovie().getTitle() + "\t"
                     + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
 
@@ -57,7 +56,6 @@ public class Customer {
 
         result += "You owed " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
-
 
         return result;
     }
